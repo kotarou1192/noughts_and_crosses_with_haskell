e19daf2cd52cde793f942574f34eb3eb470aea01
kousuke_ida <takashiii1192@gmail.com>
2022-01-25 22:46:52 +0900

    update: make possible to work test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,1 @@
-import Test.Hspec (Spec, describe, it, shouldBe)
-
-spec :: Spec
-spec = describe "output" $ do
-  it "should be" $ do
-    True `shouldBe` True
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
---
+++ test/TmpLibSpec.hs
@@ -0,0 +1,8 @@
+module TmpLibSpec where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec = describe "output" $ do
+  it "should be" $ do
+    True `shouldBe` True
